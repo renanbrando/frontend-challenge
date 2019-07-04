6ec0e41db687f1b60f924a0236e92460df102bc5
Renan Ribeiro Brando <renanbrando@Renans-MacBook-Pro-2.local>
2019-07-03 22:59:07 -0300

    Added project with webpack base.

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Helloworld/>
+  </div>
+</template>
+
+<script>
+
+import Helloworld from './components/HelloWorld.vue'
+
+export default {
+    components:{
+        Helloworld
+    },
+    data() {
+        return {
+            message: 'Hello World',
+        };
+    },
+};
+</script>
+
+<style>
+#app {
+  font-size: 18px;
+  font-family: 'Roboto', sans-serif;
+  color: blue;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>{{hello}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            hello: "Hello World"
+        }
+    }
+}
+</script>
+
